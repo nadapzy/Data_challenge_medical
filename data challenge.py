3f9838ad883747c67cabba47b12b4d2a876a328b
nadapzy <nadapzy@gmail.com>
2017-03-21 11:38:09 -0500

    npi-proc done!

--- data challenge.py
+++ data challenge.py
@@ -12,21 +12,25 @@ from nltk.corpus import stopwords
 import nltk.data
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
+from imblearn.under_sampling import RandomUnderSampler
 #nltk.download()  
 
 # Read data from files 
 npi_proc = pd.read_csv( "npi_proc_counts.csv", header=0, sep=",", quoting=2)
-grouped=npi_proc.groupby(by=['NPI','HCPCS_CODE'])
-#grouped.sum().reset_index().pivot(index='NPI',columns='HCPCS_CODE')
-grouped.sum().unstack(level=-1)
-#npi_specialty = pd.read_csv("npi_specialty.csv",header=0,sep=',',quoting=2)
+npi_specialty = pd.read_csv("npi_specialty.csv",header=0,sep=',',quoting=2)
 hcpcs = pd.read_csv("hcpcs_lookup.csv",header=0,sep=',',quoting=2)
-#npi_drugs = pd.read_csv("npi_drugs.csv",header=0,sep=',',quoting=2)
+npi_drugs = pd.read_csv("npi_drugs.csv",header=0,sep=',',quoting=2)
 
-#npi_specialty['target']=npi_specialty['specialty']=='Cardiology'
-#npi_specialty.drop(['specialty'],inplace=True,axis=1)
-#print('In total, the data have {0} cardiologists among a total of {1} physicians'\
-#.format(npi_specialty[npi_specialty.target==True].NPI.unique().size,npi_specialty.NPI.unique().size))
+npi_specialty['target']=npi_specialty['specialty']=='Cardiology'
+npi_specialty.drop(['specialty'],inplace=True,axis=1)
+print('In total, the data have {0} cardiologists among a total of {1} physicians'\
+.format(npi_specialty[npi_specialty.target==True].NPI.unique().size,npi_specialty.NPI.unique().size))
+
+#resampling until we get the ratio of cardio vs. non-cardio to 0.1
+rus=RandomUnderSampler(ratio=0.1,random_state=25,replacement=False)
+x_res,_=rus.fit_sample(npi_specialty.NPI.values.reshape(len(npi_specialty),1),npi_specialty.target.values)
+
+npi_proc=npi_proc[npi_proc.NPI.isin(x_res[:,0])]
 
 #dfnpi=npi_specialty.copy()
 #dfnpi.merge(hcpcs,how='inner',on='NPI',suffixes=('','_hcpcs'))
@@ -37,7 +41,7 @@ vectorizer = TfidfVectorizer(analyzer = "word",   \
                              tokenizer = None,lowercase=True,    \
                              preprocessor = preprocessor, \
                              stop_words =None,\
-                             max_features = 1000,\
+                             max_features = 700,\
                              ngram_range=(1,1),min_df=3)  
 #potential set binary=True
 #potentially set preprocessor to remove all numbers                              
@@ -51,12 +55,12 @@ hcpcs_bow=pd.concat([hcpcs,df_vocab],axis=1)
 
 hcpcs_bow.drop(['HCPCS_DESCRIPTION'],inplace=True,axis=1)
 
-hcpcs_bow.set_index('HCPCS_CODE')
-npi_proc.set_index('HCPCS_CODE',inplace=True)
-del hcpcs
+#hcpcs_bow.set_index('HCPCS_CODE')
+#npi_proc.set_index('HCPCS_CODE',inplace=True)
+#del hcpcs
 
 #can't run this yet, not enough memory
-#npi=npi_proc.merge(hcpcs_bow,how='inner',on='HCPCS_CODE',suffixes=('',"_hcpcs"),copy=False)
+npi=npi_proc.merge(hcpcs_bow,how='inner',on='HCPCS_CODE',suffixes=('',"_hcpcs"),copy=False)
 #potential fix: reduce the number of vocabulary
 #npi1=npi_proc.ix[:3000000,:].merge(hcpcs_bow,how='inner',on='HCPCS_CODE',suffixes=('',"_hcpcs"),copy=False)
 #npi2=npi_proc.ix[3000000:5000000,:].merge(hcpcs_bow,how='inner',on='HCPCS_CODE',suffixes=('',"_hcpcs"),copy=False)
@@ -64,14 +68,14 @@ del hcpcs
 #npi3=npi_proc.ix[7000000:,:].merge(hcpcs_bow,how='inner',on='HCPCS_CODE',suffixes=('',"_hcpcs"),copy=False)
 
 #cope with out of memory issue with python; use sqlite instead
-import sqlite3
-sqlite_file = 'my_db.sqlite'
-cxn = sqlite3.connect(sqlite_file)
-#npi_proc.to_sql(name='npi_proc',con=cxn,if_exists='replace')
+#import sqlite3
+#sqlite_file = 'my_db.sqlite'
+#cxn = sqlite3.connect(sqlite_file)
 #hcpcs_bow.to_sql(name='hcpcs',con=cxn,if_exists='replace')    #abdomina
+#npi_proc.to_sql(name='npi_proc',con=cxn,if_exists='replace')
 #resSQL='select a.NPI,a.LINE_SRVC_CNT,a.BENE_UNIQUE_CNT,b.* from npi_proc a inner join hcpcs b on a.HCPCS_CODE=b.HCPCS_CODE'
 #npi=pd.read_sql(resSQL,cxn)
-cxn.close()
+#cxn.close()
 #npi.to_csv('npi.csv',sep=',',index=False)
 
 for column in npi.columns:
@@ -80,6 +84,17 @@ for column in npi.columns:
         npi[column+'_avg']=npi[column]*npi.LINE_SRVC_CNT/npi.BENE_UNIQUE_CNT        
 
 #pivoted=npi_proc.pivot(index='NPI',columns='HCPCS_CODE')
+
+grouped=npi_proc.groupby(by=['NPI','HCPCS_CODE'])
+#grouped.sum().reset_index().pivot(index='NPI',columns='HCPCS_CODE')
+pivoted=grouped.sum().unstack(level=-1)
+
+#merge multiple level index
+pivoted.columns = [' '.join(col).strip() for col in pivoted.columns.values]
+pivoted.fillna(value=0,inplace=True)
+# now we got the npi_procedure tables transformed.
+
+
 
 
 
